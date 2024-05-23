ae0634832661b1736227632b354549317af6a6a8
karahmon <monilkarania@gmail.com>
2024-05-23 17:32:00 +0530

    dashboard fixed

--- src/layout/Dashboard.layout.tsx
+++ src/layout/Dashboard.layout.tsx
@@ -37,7 +37,7 @@ const DashboardLayout = () => {
       console.error("Error logging out:", error);
     }
   };
-
+loggedInUser && console.log("User logged in:", loggedInUser);
   return (
     <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
       <div className="hidden border-r bg-muted/40 md:block">
